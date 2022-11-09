bf5ccaee4c49819dbcac5fe72070ecefae752811
Floriian <lovasifloriian@outlook.hu>
2022-11-09 15:34:14 +0100

    Initialize Expres App.

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, () => {
+  console.log(`App is listening at port ${3000}`);
+});
