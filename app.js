afe41639857df2f5deedbab6d6d6e8ee94bf83ab
Floriian <lovasifloriian@outlook.hu>
2022-11-09 18:53:59 +0100

    trying to get working file upload

--- app.js
+++ app.js
@@ -3,22 +3,36 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const multer = require("multer");
+
+const filePath = __dirname + "/files";
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, filePath);
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   },
+// });
+// const upload = multer({ storage: storage });
 
 const app = express();
 app.use(cors());
+app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(fileUpload());
 
 app.get("/", async (req, res) => {
-  const files = await fs.readdir("files");
+  console.log(filePath);
+  const files = await fs.readdir(filePath);
   if (files.length <= 0) return res.status(404).json({ message: "No files!" });
   return res.status(200).json({ files });
 });
 
 app.get("/:filename", async (req, res) => {
   const { filename } = req.params;
-  const file = await fs.readFile(`files/${filename}.txt`, {
+  const file = await fs.readFile(`${filePath}/${filename}.txt`, {
     encoding: "utf-8",
   });
   return res.status(200).send(file);
@@ -26,32 +40,20 @@ app.get("/:filename", async (req, res) => {
 
 app.get("/d/:filename", async (req, res) => {
   const { filename } = req.params;
-  const file = await fs.readFile(`files/${filename}.txt`, {
+  const file = await fs.readFile(`${filePath}/${filename}.txt`, {
     encoding: "utf-8",
   });
   if (file.length <= 0) return res.status(404).json({ message: "No file!" });
-  return res.download(`files/${filename}.txt`);
+  return res.download(`${filePath}/${filename}.txt`);
 });
 
-app.post("/uf", async (req, res) => {
-  //TODO: upload file with POST file
-  const { files } = req;
-  console.log(req.files);
-  if (!files || Object.keys(files).length === 0) {
-    return res.status(500).json({ message: "No file agreed!" });
-  }
-
-  const file = files.vopu;
-  file.mv(`files/${file.name}`, (err) => {
-    if (err) return res.status(500).send(err);
-    return res.status(200).json({ message: "successfully uploaded file" });
-  });
-});
+//TODO
+app.post("/upload", function (req, res) {});
 
 app.post("/u/:name", async (req, res) => {
   const { content } = req.body;
   const { name } = req.params;
-  const file = fs.writeFile(`files/${name}.py`, content);
+  const file = fs.writeFile(`${filePath}/${name}.py`, content);
   if (file)
     return res.status(202).json({ message: "Successfully created file" });
   if (!file) return res.status(500).json({ message: "An error happened." });
