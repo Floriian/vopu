821da03e99ce1e8bafd01f45a9892109a6458426
Floriian <lovasifloriian@outlook.hu>
2022-11-12 11:02:25 +0100

    Delete dist directory

--- dist/routes/get.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const get_service_1 = require("../services/get.service");
-const getRouter = (0, express_1.Router)();
-getRouter.get("/", get_service_1.listFiles);
-getRouter.get("/:filename", get_service_1.getByFileName);
-getRouter.get("/d/:filename", get_service_1.downloadByFilename);
-exports.default = getRouter;
